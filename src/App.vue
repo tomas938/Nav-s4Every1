e71bdde03c4663645ac5401c7d059beb91bb3b1b
tomas938 <tomasalbert9387182@gmail.com>
2021-07-17 15:54:40 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<nav>
+		<h1>Nav's4Every1</h1>
+	</nav>
+	<div id="nav">
+		<router-link to="/">Home</router-link> |
+		<router-link to="/about">About</router-link>
+	</div>
+	<router-view />
+</template>
+
+<style>
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	font-family: "Press Start 2P", cursive;
+}
+nav {
+	height: 5rem;
+	background-color: #5d5dff;
+	color: #ffae5d;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="home">
+		<HelloWorld msg="This is Home" />
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+	name: "Home",
+	components: {
+		HelloWorld,
+	},
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
+</style>
